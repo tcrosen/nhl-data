197838c61bd377736435427f5ec172facccb039b
Terry Rosen <tcrosen@gmail.com>
2014-11-26 23:26:01 -0500

    add more game parsing

--- parse_games.js
+++ parse_games.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var fs = require('fs');
 var cheerio = require('cheerio');
 
@@ -7,7 +8,13 @@ var $ = cheerio.load(gameHtml);
 
 var selectors = {
   eventNumber: '.evenColor .bborder:nth-child(1)',
-  period: '.evenColor .bborder:nth-child(2)'
+  period: '.evenColor .bborder:nth-child(2)',
+  strength: '.evenColor .bborder:nth-child(3)',
+  times: '.evenColor .bborder:nth-child(4)',
+  eventType: '.evenColor .bborder:nth-child(5)',
+  eventDescription: '.evenColor .bborder:nth-child(6)',
+  awayOnIce: '.evenColor .bborder:nth-child(7)',
+  homeOnIce: '.evenColor .bborder:nth-child(8)'
 };
 
 var games = [];
@@ -18,8 +25,11 @@ $(selectors.eventNumber).map(function(i, el) {
   };
 });
 
-$(selectors.period).each(function(i, el) {
-  games[i].period = $(el).text();
+_.each(selectors, function(selector, key) {
+  $(selector).each(function(i, el) {
+    games[i][key] = $(el).text();
+  });
 });
+
 
 console.log(games);
